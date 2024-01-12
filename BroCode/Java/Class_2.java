51cd1b09a7a0072a280a2da4a70b0938e8a6ee8f
dhrubojyoti <lnsaha1214@gmail.com>
2024-01-12 19:47:24 +0600

    12/1/24

---
+++ BroCode/Java/Class_2.java
@@ -0,0 +1,7 @@
+package BroCode.Java;
+
+public class Class_2 {
+    public static void main(String[] args) {
+        
+    }
+}
--- Java/Hello_world.java
+++
@@ -1,6 +0,0 @@
-class Main {
-// Your program begins with a call to main().
-public static void main(String[] args) {
-System.out.println("This is a simple Java program.");
-    }
-}
