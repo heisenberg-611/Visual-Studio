669042b1f4ca24c4d56d16e79049874bef22dd81
dhrubojyoti <lnsaha1214@gmail.com>
2024-01-31 21:37:06 +0600

    31/01/24

--- Java/Hellojavap/src/App.java
+++ Java/Hellojavap/src/App.java
@@ -1,5 +1,13 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+class App{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.close();
+        int count;
+        for(count=1; count <= 5; count++){
+            System.out.println("This is count: " + count);
+            System.out.println("Done");
+        }
+
     }
 }
