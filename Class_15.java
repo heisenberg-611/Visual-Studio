0c654db50a62ad713b3c0dc3af6f93a268c19803
dhrubojyoti <lnsaha1214@gmail.com>
2025-01-17 19:32:06 +0600

    17/01/24

--- Class_15.java
+++
@@ -1,9 +0,0 @@
-public class Class_15 {
-    public static void main(String[] args) {
-        String array[] = {"Hello", "World", "Java", "is", "fun"};
-        System.out.println("The length of the array is: " + array.length);
-        for(int i = 0; i < array.length; i++){
-            System.out.println(array[i]);
-        }
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-Here I store all of my local code files to keep history.
-It's a personal space for my code
