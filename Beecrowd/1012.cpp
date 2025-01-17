3ccd4b4e04912664f05b50c3dda842400277ad21
dhrubojyoti <lnsaha1214@gmail.com>
2025-01-17 20:39:31 +0600

    17/01/24

--- Beecrowd/1012.cpp
+++ Beecrowd/1012.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    
+    double a,b,c;
+    cin>>a>>b>>c;
+    cout.precision(3);
+    cout.setf(ios::fixed);
+    cout<<"TRIANGULO: "<<(a*c)/2<<endl;
+    cout<<"CIRCULO: "<<3.14159*c*c<<endl;
+    cout<<"TRAPEZIO: "<<((a+b)*c)/2<<endl;
+    cout<<"QUADRADO: "<<b*b<<endl;
+    cout<<"RETANGULO: "<<a*b<<endl;
+    return 0;
 }
---
+++ Beecrowd/Area.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+        double pi = 3.14159;
+        double TRIANGULO = (A * C) / 2;
+        double CIRCULO = pi * C * C;
+        double TRAPEZIO = ((A + B) * C) / 2;
+        double QUADRADO = B * B;
+        double RETANGULO = A * B;
+        System.out.println("TRIANGULO: " + String.format("%.3f", TRIANGULO));
+        System.out.println("CIRCULO: " + String.format("%.3f", CIRCULO));
+        System.out.println("TRAPEZIO: " + String.format("%.3f", TRAPEZIO));
+        System.out.println("QUADRADO: " + String.format("%.3f", QUADRADO));
+        System.out.println("RETANGULO: " + String.format("%.3f", RETANGULO));
+        sc.close();
+    }
+}
