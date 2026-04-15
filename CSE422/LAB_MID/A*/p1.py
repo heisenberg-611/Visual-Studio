c98bf9ce2d188a9fea760b4690ccfbeda17c1c14
heisenberg-611 <lnsaha1214@gmail.com>
2026-04-15 08:42:38 +0600

    xD

--- CSE422/LAB_MID/A*/p1.py
+++ CSE422/LAB_MID/A*/p1.py
@@ -1,43 +1,168 @@
+# import heapq
+
+# direction = [
+#     (-1, 0, 'U'),
+#     (1, 0, 'D'),
+#     (0, -1, 'L'),
+#     (0, 1, 'R')
+# ]
+
+# def manhattan(x1, x2, y1, y2):
+#     return abs(x1-x2) + abs(y1-y2)
+
+# def astar(n, m, start, goal, maze):
+#     visited = set()
+#     pq = []
+#     sx, sy = start
+#     gx, gy = goal
+
+#     h = manhattan(sx, sy, gx, gy)
+#     heapq.heappush(pq, (h, 0, sx, sy, ""))
+
+#     while pq:
+#         f, g, x, y, path = heapq.heappop(pq)
+
+#         if(x, y) in visited:
+#             continue
+
+#         visited.add((x, y))
+
+#         if(x, y) == (gx, gy):
+#             return g, path
+
+#         for dx, dy, move in direction:
+#             nx, ny = x+dx, y+dy
+
+#             if 0 <= nx < n and 0 <= ny < m:
+#                 if maze[nx][ny] == '0' and (nx)(ny) not in visited:
+#                     new_g = g+1
+#                     new_h = manhattan(nx, ny, gx, gy)
+#                     new_f = new_g + new_h
+
+#                     heapq.heappush(pq,(new_f, new_g, nx, ny, path+move))
+#     return -1, ""
+
+# n, m = map(int, input().split())
+# sx, sy = map(int, input().split())
+# gx, gy = map(int, input().split())
+
+# maze = [input().strip() for _ in range(n)]
+
+# cost, path = astar(n, m, (sx, sy), (gx, gy), maze)
+
+# if cost == -1:
+#     print(-1)
+# else:
+#     print("Total step: ", cost)
+#     print(path)
+
+
+# import heapq
+
+# direction = [
+#     (-1, 0, 'U'),
+#     (1, 0, 'D'),
+#     (0, 1, 'R'),
+#     (0, -1, 'L')
+# ]
+
+# def manhattan(x1, x2, y1, y2):
+#     return abs(x1-x2) + abs(y1 - y2)
+
+# def astar(n, m, start, goal, maze):
+#     pq =[]
+#     visited = set()
+#     sx, sy = start
+#     gx, gy = goal
+
+#     h = manhattan(sx,sy,gx,gy)
+#     heapq.heappush(pq,(h, 0, sx, sy, ""))
+
+#     while pq:
+#         f, g, x, y, path = heapq.heappop(pq)
+#         if(x, y) in visited:
+#             continue
+#         visited.add(x, y)
+#         if((x,y) == (gx, gy)):
+#             return g, path
+        
+#         for dx, dy, move in direction:
+#             nx , ny = dx+x, dy+y;
+
+#             if(0 <= nx <n  and 0 <= ny < m ):
+#                 if maze[nx,ny] == '0'and (nx)(ny) not in visited:
+#                     new_g = g+1
+#                     new_h = manhattan(nx, ny, gx,gy)
+#                     new_f = new_h + new_g
+
+#                     heapq.heappush(pq, (new_f, new_g, nx, ny, path+move))
+#     return -1, ""
+
+# n, m = map(int, input().split())
+# sx, sy = map(int, input().split())
+# gx, gy = map(int, input().split())
+
+# maze = [input().strip() for _ in range(n)]
+
+# cost, path = astar(n, m, (sx, sy), (gx,gy), maze)
+
+# if(cost == -1):
+#     print("-1")
+# else:
+    
+
 import heapq
 
 direction = [
-    (-1, 0, 'U'),
+    (-1, 0 , 'U'),
     (1, 0, 'D'),
     (0, -1, 'L'),
     (0, 1, 'R')
 ]
 
-def manhattan(x1, x2, y1, y2):
-    return abs(x1-x2) + abs(y1-y2)
+def manhattan (x1, y1, x2, y2):
+    return abs(x1-x2)+abs(y1-y2)
 
 def astar(n, m, start, goal, maze):
+    pq=[]
     visited = set()
-    pq = []
     sx, sy = start
     gx, gy = goal
 
     h = manhattan(sx, sy, gx, gy)
-    heapq.heappush(pq, (h, 0, sx, sy, ""))
+    heapq.heappush(pq, (h, 0, sx, sy, "" ))
 
     while pq:
         f, g, x, y, path = heapq.heappop(pq)
-
-        if(x, y) in visited:
+        if(x,y) in visited:
             continue
-
-        visited.add((x, y))
-
-        if(x, y) == (gx, gy):
+        visited.add((x,y))
+        if((x, y) == (gx, gy)):
             return g, path
-
+        
         for dx, dy, move in direction:
-            nx, ny = x+dx, y+dy
+            nx = x + dx
+            ny = y + dy
 
-            if 0 <= nx < n and 0 <= ny < m:
-                if maze[nx][ny] == '0' and (nx)(ny) not in visited:
+            if(0 <= nx < n) and (0 <= ny <m):
+                if(maze[nx][ny] == '0' and (nx,ny) not in visited):
                     new_g = g+1
-                    new_h = manhattan(nx, ny, gx, gy)
-                    new_f = new_g + new_h
+                    new_h = manhattan(nx, ny, gx,gy)
+                    new_f = new_h+new_g
 
-                    heapq.heappush(pq,(new_f, new_g, nx, ny, path+move))
+                    heapq.heappush(pq, (new_f, new_g, nx, ny, path+move))
     return -1, ""
+
+n, m = map(int, input().split())
+sx, sy = map(int, input().split())
+gx, gy = map(int, input().split())
+
+maze = [input().strip() for _ in range(n)]
+
+cost, path = astar(n, m, (sx, sy), (gx,gy), maze)
+
+if(cost == -1):
+    print(-1)
+else:
+    print(path)
+    print(cost)
